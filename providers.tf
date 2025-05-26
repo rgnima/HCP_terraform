4b26e7e2dd9a8850aafd7594f5ce29ab818a9d4e
Ricardo <ricardonima@gmail.com>
2025-05-25 22:57:31 -0500

    s3

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+
+  required_version = ">= 1.10.0"
+}
+provider "aws" {
+  region = "us-east-1"
+    access_key = var.access_key
+    secret_key = var.access_key
+}

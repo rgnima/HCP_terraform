e21dc7ae08685645d409a4d1e3fa2b70f0007948
Ricardo <ricardonima@gmail.com>
2025-05-25 22:39:34 -0500

    s3

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "s3" {
+  source  = "app.terraform.io/rgnima-cloud-org/s3/aws"
+  version = "0.0.1"
+  # insert required variables here
+  project_name = "asob"
+  env = "cert"
+  nram = "5"
+}

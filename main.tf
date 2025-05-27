8c693b1b8d17ba7472fae01be3c14e798db86098
Ricardo <ricardonima@gmail.com>
2025-05-27 01:00:28 -0500

    test module 0.0.2

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-# module "s3" {
-#   source  = "app.terraform.io/rgnima-cloud-org/s3/aws"
-#   version = "0.0.1"
-#   # insert required variables here
-#   project_name = "asob"
-#   env = "cert"
-#   nrandom = 4
+module "s3" {
+  source  = "app.terraform.io/rgnima-cloud-org/s3/aws"
+  version = "0.0.2"
+  # insert required variables here
+  project_name = "bizfw"
+  env = "prod"
+  
 
-# }
+}
